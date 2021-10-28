dc84b1a582ac1324ed68d930320af087e6b64d55
petr-micola <petamicol@gmail.com>
2021-10-28 09:32:56 +0200

    Základy webu

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById('toTop');
+const btn2 = document.getElementById('darkToggle');
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btn.classList.add('visible');
+    } else {
+        btn.classList.remove('visible');
+    }
+}
+
+btn.addEventListener('click', function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+btn2.addEventListener('click', function(){
+    document.body.classList.toggle('dark');
+})
